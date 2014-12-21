e75c6eff22164b07e21fa71a41719818fa701f37
lmc70 <limingchen70@yahoo.com>
2014-12-20 20:36:57 -0600

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    s <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(solve) s <<- solve 
+    
+    getinverse <- function() s
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
@@ -11,5 +28,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    s <- x$getinverse()
+    
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    
+    data <- x$get()
+    s <- solve(data, ...)
+    
+    x$setinverse(s)
+    
+    ## Return a matrix that is the inverse of 'x'
+    
+    s       
 }
